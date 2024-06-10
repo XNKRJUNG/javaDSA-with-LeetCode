e5d9f53f924760b78496e8fa88515ed414d06f79
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-06-10 13:21:43 -0400

    DSA: LinkedList: Get

---
+++ src/main/java/org/cyrus/javaDSA/linkedList/get/Main.java
@@ -0,0 +1,25 @@
+package org.cyrus.javaDSA.linkedList.get;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+
+        System.out.println(myLinkedList.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+    }
+
+}
